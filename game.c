2f198cbc4701854b1be2e2a4348c30feb7867532
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2018-08-02 18:55:36 +0530

    Snake doesn't eat itself
    Game gets over when the snake tries to eat itself.
    The program structure has also been overhauled. Many parts of the
    code have been extracted to functions to better convey their functionality.

--- game.c
+++ game.c
@@ -1,19 +1,47 @@
 /*
  * Copyright (c) Abhyudaya Sharma, 2018
  */
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <curses.h>
 #include <string.h>
 
-#define SNAKE_HEAD ACS_DIAMOND
-#define SNAKE_LEN 10
+#define SNAKE_HEAD ACS_DIAMOND 
+#define SNAKE_LEN 20
 #define ADD_POINT(point) do { mvaddch(point.y, point.x, ACS_BLOCK); } while (0)
 
 struct Point {
     int y;
     int x;
 };
+
+inline bool arePointsEqual(struct Point x, struct Point y) {
+    return x.x == y.x && x.y == y.y;
+}
+
+void initCurses() {
+    initscr();
+    raw();
+    noecho();
+    clear();
+    keypad(stdscr, TRUE);
+    curs_set(0); // Hide the cursor
+}
+
+// Acts as a splash screen
+bool runGame() {
+    printw("Hello World.\nA simple movement tool.\nPress 'q' to quit or press any other key to continue...");
+    refresh();
+    int ch = getch();
+
+    if (ch == 'q') {
+        return false;
+    } else {
+        return true;
+    }
+
+}
 
 inline bool isInBounds(int y, int x) {
     int maxX, maxY;
@@ -25,20 +53,44 @@ inline bool isInBounds(int y, int x) {
     }
 }
 
-void moveSnake(struct Point* points, int snakeLen, int dy, int dx) {
+bool isOverlapped(struct Point p, struct Point* points, int snakeLen) {
+    for (int i = 0; i < snakeLen; i++) {
+        if (arePointsEqual(p, points[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool moveSnake(struct Point* points, int snakeLen, int dy, int dx) {
+    // restrict moving off the screen
+    if (!isInBounds(points[0].y + dy, points[0].x + dx)) {
+        return true; // Reaching the bounds does not mean the end of the game.
+    }
+
+    // check overlap
+    if (isOverlapped((struct Point) {points[0].y + dy, points[0].x + dx}, points, snakeLen)) {
+        return false;
+    }
+
     clear();
+
+    // add snake's body points
     for (int i = snakeLen- 1; i > 0; i--) {
         points[i].y = points[i - 1].y;
         points[i].x = points[i - 1].x;
 
         ADD_POINT(points[i]);
     }
+
     points[0].y += dy;
     points[0].x += dx;
-    mvaddch(points[0].y, points[0].x, SNAKE_HEAD); 
+    mvaddch(points[0].y, points[0].x, SNAKE_HEAD);
+    return true;
 }
 
-void resetCursor(int* y, int* x, struct Point* points, int snakeLen) {
+void resetSnake(struct Point* points, int snakeLen) {
     clear();
     int maxX, maxY;
     getmaxyx(stdscr, maxY, maxX);
@@ -46,87 +98,94 @@ void resetCursor(int* y, int* x, struct Point* points, int snakeLen) {
     int curX = maxX / 2;
     int curY = maxY / 2;
 
-    mvaddch(curY, curX, SNAKE_HEAD); 
+    // Snake head
+    mvaddch(curY, curX, SNAKE_HEAD);
     points[0].y = curY;
     points[0].x = curX;
+
+    // Snake body
     for (int i = 1; i < snakeLen; i++) {
         points[i].y = points[i - 1].y;
         points[i].x = points[i - 1].x - 1;
         ADD_POINT(points[i]);
     }
 
-    // Update the locations
-    *y = curY;
-    *x = curX;
     refresh();
 }
 
-int main(int argc, char ** argv) {
-    // Init curses
-    initscr();
-    raw();
-    noecho();
-    clear();
-    keypad(stdscr, TRUE);
-    curs_set(0); // Hide the cursor
-
-    // Start here
-    printw("Hello World.\nA simple movement tool.\nPress 'q' to quit.\nPress any key to continue...");
-    refresh();
-    int ch = getch();
-
-    switch(ch) {
-        case KEY_F(1):
-            attron(A_BOLD);
-            addstr("\nYou pressed F1");
-            attroff(A_BOLD);
-            refresh();
-            break;
-        case 'q':
-            endwin();
-            return 0;
-    }
-
-    int curY, curX;
-
+void loop() {
     // head is points[0]
     struct Point points[SNAKE_LEN];
-    resetCursor(&curY, &curX, points, SNAKE_LEN);
+    resetSnake(points, SNAKE_LEN);
 
     bool flag = true;
+    int direction = 0;
+    bool overlap = false;
+
     while (flag) {
         // Check for terminal size change
-        /*
-        if (!isInBounds(curY, curX)) {
-            clear();
-            resetCursor(&curY, &curX, points, SNAKE_LEN);
-            refresh(); 
+        if (!isInBounds(points[0].y, points[0].x)) {
+            resetSnake(points, SNAKE_LEN);
         }
-        */
-        
+
         int input = getch();
         switch (input) {
             case KEY_UP:
-                moveSnake(points, SNAKE_LEN, -1, 0);
+                if (direction != KEY_DOWN) {
+                    overlap = !moveSnake(points, SNAKE_LEN, -1, 0);
+                    direction = input;
+                }
                 break;
             case KEY_DOWN:
-                moveSnake(points, SNAKE_LEN, 1, 0);
+                if (direction != KEY_UP) {
+                    overlap = !moveSnake(points, SNAKE_LEN, 1, 0);
+                    direction = input;
+                }
                 break;
             case KEY_LEFT:
-                moveSnake(points, SNAKE_LEN, 0, -1);
+                if (direction != KEY_RIGHT) {
+                    overlap = !moveSnake(points, SNAKE_LEN, 0, -1);
+                    direction = input;
+                }
                 break;
             case KEY_RIGHT:
-                moveSnake(points, SNAKE_LEN, 0, 1);
+                if (direction != KEY_LEFT) {
+                    overlap = !moveSnake(points, SNAKE_LEN, 0, 1);
+                    direction = input;
+                }
                 break;
             case 'q':
                 flag = false;
                 break;
         }
+
+        if (overlap) {
+            flag = false;
+        }
         refresh();
+    }
+
+    if (overlap) {
+        clear();
+        addstr("Game Over!\nYour snake ate itself!\nPress any key to exit...");
+        refresh();
+        getch();
     }
 
     // Close window
     endwin();
+}
+
+int main(int argc, char ** argv) {
+    // Init curses
+    initCurses();
+
+    if (runGame()) {
+        /* Main logic of the program */
+        loop();
+    }
+
+    endwin(); // cleanup curses
     return 0;
 }
 
